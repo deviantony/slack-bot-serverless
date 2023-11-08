58c4500861ec096042e1d52c42ebf8ad998fdc18
deviantony <anthony.lapenna@portainer.io>
2023-11-08 01:47:15 +0000

    feat: debug the event object

--- packages/slack/msg/msg.go
+++ packages/slack/msg/msg.go
@@ -1,12 +1,17 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type Event struct {
 	Message string `json:"message"`
 }
 
 func Main(ctx context.Context, event Event) map[string]interface{} {
+	fmt.Printf("Received event: %+v\n", event)
+
 	name := event.Message
 	if name == "" {
 		name = "World"
